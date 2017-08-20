73f72a5feb3ab48f266a72528194f779c6549416
bwnyasse <risbonyasse@gmail.com>
2017-08-20 18:33:00 +0200

    [m_s] parent widget manage widget's state

--- managing_state/lib/main.dart
+++ managing_state/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/foundation.dart';
 
 // TapboxA manages its own state.
 
@@ -40,6 +40,68 @@ class _TapboxAState extends State<TapboxA> {
   }
 }
 
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+// ParentWidget manages the state for TapboxB.
+
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
 //------------------------- My App ------------------------------
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -52,7 +114,13 @@ class MyApp extends StatelessWidget {
           title: new Text('Flutter Managing State Demo'),
         ),
         body: new Center(
-          child: new TapboxA(),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new TapboxA(),
+              new ParentWidget()
+            ],
+          ),
         ),
       )
     );
